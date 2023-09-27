22984680e1ef996bd9389343acfc6dcfebf580b5
sobolevadd <dasha.soboleva.0.3.0@gmail.com>
2023-09-27 17:51:30 +0200

    commit

--- PRC1/cvika/cviko1/Banknotes/Banknotes.cpp
+++ PRC1/cvika/cviko1/Banknotes/Banknotes.cpp
@@ -40,7 +40,7 @@ int main()
 using namespace std;
 enum {OK};
 
-
+unsigned const int N = 12;
 unsigned int money[N]= {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
 
 void calculate(unsigned int value,unsigned int *res, unsigned int *notes, int n ) 
